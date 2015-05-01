6f1de34ab001890530db5cb303447227d6f8f9b8
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-05-01 16:31:59 +0100

    Add form

--- index.php
+++ index.php
@@ -2,9 +2,41 @@
 <html lang="en">
 <head>
 	<title>Wedding Planner Southampton</title>
-	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
-</head>
+	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css"></head>
 <body>
+	<h1 class="text-center">Wedding Planner Service</h1>
+	<div class="container">
+		<form class="form-horizontal">
+			<div class="form-group">
+				<label for="firstName">First Name</label>
+				<input type="text" placeholder="" id="firstName"></div>
+			<div class="form-group">
+				<label for="lastName">Last Name</label>
+				<input type="text" placeholder="" id=""></div>
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="email" placeholder="" id=""></div>
+			<div class="form-group">
+				<label for="address">Address</label>
+				<input type="text" placeholder="" id=""></div>
+			<div class="form-group">
+				<label for="postCode">Postcode</label>
+				<input type="text" placeholder="" id=""></div>
+			<div class="form-group">
+				<label for="telephone">Telephone</label>
+				<input type="tel" placeholder="" id=""></div>
+			<div class="form-group">
+				<label for="weddingDate">Wedding Date</label>
+				<input type="date" placeholder="" id=""></div>
+			<div class="form-group">
+				<label for="weddingLocation">Wedding Location</label>
+				<input type="text" placeholder="" id=""></div>
+			<div class="form-group">
+				<label for="specialRequirements">Special requirements</label>
+				<input type="text" placeholder="" id=""></div>
+		</form>
+	</div>
+
 	<script type="text/javascript" src="lib/jquery/jquery-2.1.3.min.js"></script>
 	<script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
