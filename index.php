3c8a6b98475e60948c008b146558be70d78e19f1
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-05-01 16:12:53 +0100

    Add main page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Wedding Planner Southampton</title>
+	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<script type="text/javascript" src="lib/jquery/jquery-2.1.3.min.js"></script>
+	<script type="text/javascript" src="lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
