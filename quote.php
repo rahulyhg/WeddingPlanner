1442bd2b8e9843acad47727671652a26f16f4512
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-05-01 21:26:07 +0100

    Fix problem of not adding values to database and add extra catch and block for error displaying

--- quote.php
+++ quote.php
@@ -1,19 +1,9 @@
 <?php
     require ('connect_oo.php'); #connect to database
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     #values initialised
     $today = date("Y-m-d H:i:s");    
-    $firstName = "";
-    $lastName = "";
-    $email = "";
-    $address = "";
-    $postcode = "";
-    $telephone = "";
-    $weddingDate = "";
-    $weddingLocation = "";
-    $specialRequirements = "";
-
-
+    
     $firstName = $mysqli->real_escape_string($_POST['firstName']);
     $lastName = $mysqli->real_escape_string($_POST['lastName']);
     $email = $mysqli->real_escape_string($_POST['email']);
@@ -23,28 +13,43 @@
     $weddingDate = $mysqli->real_escape_string($_POST['weddingDate']);
     $weddingLocation = $mysqli->real_escape_string($_POST['weddingLocation']);
     $specialRequirements = $mysqli->real_escape_string($_POST['specialRequirements']);
+    $status = "current";
+    $gender = "M";
 
-    echo $firstName +"\n";
-    echo $lastName+"\n";
-    echo $email+"\n";
-    echo $address+"\n";
-    echo $postcode+"\n";
-    echo $telephone+"\n";
-    echo $weddingDate+"\n";
-    echo $weddingLocation+"\n";
-    echo $specialRequirements+"\n";
-    echo $today+"\n";
-
+    echo "
+<ul>
+    <li>$firstName</li>
+    <li>$lastName</li>
+    <li>$email</li>
+    <li>$address</li>
+    <li>$postcode</li>
+    <li>$telephone</li>
+    <li>$weddingDate</li>
+    <li>$weddingLocation</li>
+    <li>$specialRequirements</li>
+    <li>$today</li>
+</ul>
+";
+   
     #query
-        $stmt = $mysqli->prepare("INSERT INTO inquiries(submit_date, first_name,last_name,address,postcode,email,phone,wedding_date,wedding_location,special_req) VALUES (?,?,?,?,?,?,?,?,?,?);");
-        $stmt->bind_param('ssssssssss', $today, $firstName, $lastName, $address, $postcode, $email, $telephone, $weddingDate, $weddingLocation, $specialRequirements);
-        $OK = $stmt->execute();
-        #if query has been executed
+        if($stmt = $mysqli->prepare("INSERT INTO inquiries(submit_date, first_name, last_name, address, postcode, email, phone, wedding_date, wedding_location, special_req) VALUES (?,?,?,?,?,?,?,?,?,?);")){
+            $stmt->bind_param('ssssssssss', $today, $firstName, $lastName, $address, $postcode, $email, $telephone, $weddingDate, $weddingLocation, $specialRequirements);
+            $OK = $stmt->execute();
+                    #if query has been executed
         if ($OK)
-            echo "<p>Student has been enrolled</p>";
+            echo "
+<p>Your quote has been processed</p>
+";
         else
-            echo "<p> Something went wrong :(</p>";
+            echo "
+<p>Something went wrong :(</p>
+";
 
         $stmt->close();
+
+        }else{
+             printf("Errormessage: %s\n", $mysqli->error);
+        }
         $mysqli->close();
+    }
 ?>
