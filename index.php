0ea1d471a0cf21d0e65deab662ecf1915a6a6b0f
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-05-01 16:43:04 +0100

    Add all the things for form

--- index.php
+++ index.php
@@ -9,31 +9,52 @@
 		<form class="form-horizontal">
 			<div class="form-group">
 				<label for="firstName">First Name</label>
-				<input type="text" placeholder="" id="firstName"></div>
+				<div>
+					<input type="text" placeholder="" id="firstName"></div>
+			</div>
 			<div class="form-group">
 				<label for="lastName">Last Name</label>
-				<input type="text" placeholder="" id=""></div>
+				<div>
+					<input type="text" placeholder="" id="lastName"></div>
+			</div>
 			<div class="form-group">
 				<label for="email">Email</label>
-				<input type="email" placeholder="" id=""></div>
+				<div>
+					<input type="email" placeholder="" id="email"></div>
+			</div>
 			<div class="form-group">
 				<label for="address">Address</label>
-				<input type="text" placeholder="" id=""></div>
+				<div>
+					<input type="text" placeholder="" id="address"></div>
+			</div>
 			<div class="form-group">
 				<label for="postCode">Postcode</label>
-				<input type="text" placeholder="" id=""></div>
+				<div>
+					<input type="text" placeholder="" id="postCode"></div>
+			</div>
 			<div class="form-group">
 				<label for="telephone">Telephone</label>
-				<input type="tel" placeholder="" id=""></div>
+				<div>
+					<input type="tel" placeholder="" id="telephone"></div>
+			</div>
 			<div class="form-group">
 				<label for="weddingDate">Wedding Date</label>
-				<input type="date" placeholder="" id=""></div>
+				<div>
+					<input type="date" placeholder="" id="weddingDate"></div>
+			</div>
 			<div class="form-group">
 				<label for="weddingLocation">Wedding Location</label>
-				<input type="text" placeholder="" id=""></div>
+				<div>
+					<input type="text" placeholder="" id="weddingLocation"></div>
+			</div>
 			<div class="form-group">
 				<label for="specialRequirements">Special requirements</label>
-				<input type="text" placeholder="" id=""></div>
+				<div>
+					<input type="text" placeholder="" id="specialRequirements"></div>
+			</div>
+			<div class="form-group">
+				<button class="btn btn-default">Submit enquiry</button>
+			</div>
 		</form>
 	</div>
 
