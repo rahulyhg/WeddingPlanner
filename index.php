911f080231b040e42481b94a622aeefebf8803ef
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-05-01 17:25:23 +0100

    * Add css file
    * Rescale images
    * Reposition elements on page

--- index.php
+++ index.php
@@ -3,45 +3,51 @@
 <head>
 	<title>Wedding Planner Southampton</title>
 	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css"></head>
+	<link rel="stylesheet" href="css/main.css"></head>
 <body>
 	<h1 class="text-center">Wedding Planner Service</h1>
 	<div class="container">
-
+	<div class="row">
+		<div class="col-md-8">
 		<!-- Carousel of images -->
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<div id="weddingPicsCarousel" class="carousel slide" data-ride="carousel">
 			<!-- Indicators -->
 			<ol class="carousel-indicators">
-				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				<li data-target="#weddingImage1" data-slide-to="0" class="active"></li>
+				<li data-target="#weddingImage2" data-slide-to="1"></li>
+				<li data-target="#weddingImage4" data-slide-to="2"></li>
 			</ol>
 
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner" role="listbox">
 				<div class="item active">
-					<img src="..." alt="...">
+					<img class="img-responsive" src="img/weddingImage1.jpg" alt="Generic Wedding Image">
+					<div class="carousel-caption">Plannign Weddings is tough!</div>
+				</div>
+				<div class="item">
+					<img class="img-responsive" src="img/weddingImage2.jpg" alt="Generic Wedding Image">
 					<div class="carousel-caption">...</div>
 				</div>
 				<div class="item">
-					<img src="..." alt="...">
+					<img class="img-responsive" src="img/weddingImage3.jpg" alt="Generic Wedding Image">
 					<div class="carousel-caption">...</div>
 				</div>
-				...
 			</div>
 
 			<!-- Controls -->
-			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			<a class="left carousel-control" href="#weddingPicsCarousel" role="button" data-slide="prev">
 				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 				<span class="sr-only">Previous</span>
 			</a>
-			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			<a class="right carousel-control" href="#weddingPicsCarousel" role="button" data-slide="next">
 				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 				<span class="sr-only">Next</span>
 			</a>
 		</div>
+		</div>
 
 		<!-- Quote form -->
-		<div>
+		<div class="col-md-4">
 			<h3 class="text-center">Get a Quote</h3>
 			<form class="form-horizontal">
 				<div class="form-group">
@@ -93,6 +99,7 @@
 					<button type="submit" class="btn btn-default btn-block">Submit enquiry</button>
 				</div>
 			</form>
+		</div>
 		</div>
 	</div>
 
